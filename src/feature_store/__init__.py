f56ac230dd17873a8153d8fb01129d7c5e4202af
kareemxmhmd <kareemxmhmd@gmail.com>
2026-09-01 14:26:53 +0300

    feat: Add Enterprise Feature Store and Serving modules with initial structure

---
+++ src/feature_store/__init__.py
@@ -0,0 +1,7 @@
+﻿"""
+Enterprise Feature Store module for online entity hydration and offline feature retrieval.
+"""
+
+from src.feature_store.store import EnterpriseFeatureStore, FeatureDefinition, FeatureView
+
+__all__ = ["EnterpriseFeatureStore", "FeatureDefinition", "FeatureView"]
---
+++ src/serving/__init__.py
@@ -0,0 +1,7 @@
+﻿"""
+Enterprise Serving package for traffic routing and experimentation.
+"""
+
+from src.serving.router import ModelRouter, RoutingDecision
+
+__all__ = ["ModelRouter", "RoutingDecision"]
